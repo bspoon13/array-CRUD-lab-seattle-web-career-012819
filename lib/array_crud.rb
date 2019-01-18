92ba3f6605cd65b1cee17323717a0ed046a60200
Billy Witherspoon <wwither@gmail.com>
2019-01-18 00:23:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  dreamcars = [corvette, ferrari, tesla, lambo]
+  dreamcars = ["corvette", "ferrari", "tesla", "lambo"]
 end
 
 def add_element_to_end_of_array(array, element)
